4a707a353eb05015f64784bc2c4c057719b98308
muradorucov <orucowmurad@gmail.com>
2022-05-01 19:10:21 +0400

    Btn active class update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 let btns = document.querySelectorAll('.currency-types li');
 
+const currency = {
+    USD: 1.7,
+    EUR: 2,
+    RUB: 0.2,
+    GBP: 1.2
+}
+
+
 btns.forEach((item, index) => {
     item.addEventListener('click', (e) => {
-        item.className = 'active';
+        let childBtn = e.target.parentElement.children
+        for (let i = 0; i < childBtn.length; i++) {
+            childBtn[i].classList.remove("active")
+        }
+        e.target.classList.add('active')
+        if (item.innerHTML == 'USD') {
+            console.log(currency.USD)
+        }
     })
 })
+
+
+// https: //api.exchangerate.host/latest
